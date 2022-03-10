e6a9df42f4c590cbbc4a5bbdc99293eb28ea82ba
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-03-11 00:02:17 +0100

    Fix typo for environ keys object

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const pgClient = new Pool({
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
-  port: key.pgPort,
+  port: keys.pgPort,
 });
 
 pgClient.on("connect", (client) => {
