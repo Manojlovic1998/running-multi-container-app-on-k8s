9d4ed9d6ea1da22a046f5ccd38e453d668ec0768
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-03-14 12:24:04 +0100

    Create navigation component

---
+++ client/src/Components/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { Navbar, Container, Row, Col } from "react-bootstrap";
+import NavSvgLogo from "../../assets/svg/nav-logo.svg";
+const Navigation = () => {
+  return (
+    <Container fluid className={"px-0 shadow-sm"}>
+      <Navbar className={"w-100 py-3"} bg={"light"}>
+        <Link
+          to="/"
+          className={"fw-bolder text-black fs-5 mx-auto text-decoration-none"}
+        >
+          <Row>
+            <Col>
+              Fibonacci <br />
+              Sequence
+            </Col>
+            <Col className={"d-flex"}>
+              <img src={NavSvgLogo} className={"my-auto"} />
+            </Col>
+          </Row>
+        </Link>
+      </Navbar>
+    </Container>
+  );
+};
+
+export default Navigation;
