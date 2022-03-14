e2af6bbea547ef9b9f77c11a43371490f415cdd0
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-03-14 14:00:48 +0100

    Render Fib component in main section of index template

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./Components/Navigation/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Fib from "./Pages/Fib";
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
@@ -15,7 +15,7 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("header")
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+ReactDOM.render(<Fib />, document.getElementById("main"));
+
 reportWebVitals();
