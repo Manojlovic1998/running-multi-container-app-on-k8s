85c301a711c10bea040d4adab2d16729d790ea23
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-03-14 13:57:31 +0100

    Create new form layout using bootstrap

--- client/src/Components/Forms/IndexForm.js
+++ client/src/Components/Forms/IndexForm.js
@@ -1,19 +1,34 @@
 import React from "react";
+import { Form, Button, Col } from "react-bootstrap";
 
 const IndexForm = (props) => {
   return (
-    <div>
-      <form onSubmit={props.onIndexSubmit}>
-        <label htmlFor="index">Enter your index:</label>
-        <input
-          type="text"
-          id="index"
-          value={props.index}
-          onChange={(event) => props.setIndex(event.target.value)}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
+    <Col
+      lg={4}
+      md={5}
+      sm={7}
+      className={"mx-auto shadow-lg py-4 px-4 rounded-3 border bg-white"}
+    >
+      <h1 className={"fs-5 mb-4 mt-3"}>Fibonacci Sequence Calculator</h1>
+      <Form onSubmit={props.onIndexSubmit}>
+        <Form.Group>
+          <Form.Label htmlFor="index">Enter your index:</Form.Label>
+          <Form.Control
+            type="text"
+            id="index"
+            value={props.index}
+            onChange={(event) => props.setIndex(event.target.value)}
+          />
+          <Form.Text>
+            Enter the index of the number in fib sequence that you are looking
+            for
+          </Form.Text>
+        </Form.Group>
+        <div className={"text-end text-lg-center"}>
+          <Button className={"mt-3"}>Submit</Button>
+        </div>
+      </Form>
+    </Col>
   );
 };
 
