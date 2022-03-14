7198c8ade4412b6574a76f8145cdeaab694772b4
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-03-14 12:30:45 +0100

    Add Navigation to the header

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./Components/Navigation/Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Navigation />} />
     </Routes>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById("header")
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
