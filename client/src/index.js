df5ca6821baf297f522d05efeef0e86337bd14c3
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-03-15 19:30:46 +0100

    Fix app render

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./Components/Navigation/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Fib from "./Pages/Fib";
+
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
@@ -16,6 +16,6 @@ ReactDOM.render(
   document.getElementById("header")
 );
 
-ReactDOM.render(<Fib />, document.getElementById("main"));
+ReactDOM.render(<App />, document.getElementById("main"));
 
 reportWebVitals();
